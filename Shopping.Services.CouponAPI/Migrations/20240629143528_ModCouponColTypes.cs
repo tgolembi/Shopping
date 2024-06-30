7d5a2965bd9499aded43cc492d42ce9cbb4cf53a
Ton Golembiouski <tgolembi@gmail.com>
2024-06-30 16:37:31 -0300

    Copuon MS - Initial Migrations Coupon and Seed

---
+++ Shopping.Services.CouponAPI/Migrations/20240629143528_ModCouponColTypes.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shopping.Services.CouponAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModCouponColTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MinAmount",
+                table: "Coupons",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "DiscountAmount",
+                table: "Coupons",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MinAmount",
+                table: "Coupons",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DiscountAmount",
+                table: "Coupons",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
