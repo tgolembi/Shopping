fd04900420f576783ce9fa7426257abde9dc59c3
Ton Golembiouski <tgolembi@gmail.com>
2024-06-30 16:35:58 -0300

    Coupon MS - New Coupon Controller and ResponseDTO

---
+++ Shopping.Services.CouponAPI/Controllers/CouponController.cs
@@ -0,0 +1,171 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shopping.Services.CouponAPI.Data;
+using Shopping.Services.CouponAPI.Models;
+using Shopping.Services.CouponAPI.Models.DTO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shopping.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponController : ControllerBase
+    {
+        private readonly AppDbContext _dbContext;
+        private ResponseDTO _response = new ResponseDTO();
+        private readonly IMapper _mapper;
+
+        public CouponController (AppDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ResponseDTO Get ()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _dbContext.Coupons.ToList();
+                _response.Result = _mapper.Map<IEnumerable<CouponDTO>>(objList);
+                _response.Success = true;
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.GetType().ToString() + " ::: " + ex.Message; //TODO: Change message
+            }
+
+            return _response;
+        }
+
+        [HttpGet("{id:int}")]
+        public ResponseDTO Get (int id)
+        {
+            try
+            {
+                Coupon? obj = _dbContext.Coupons.FirstOrDefault(c => c.CouponId == id);
+                
+                if (obj == null)
+                {
+                    _response.Message = $"Coupon [{id}] not found";
+                }
+                else
+                {
+                    _response.Result = _mapper.Map<CouponDTO>(obj);
+                    _response.Success = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.GetType().ToString() + " ::: " + ex.Message; //TODO: Change message
+            }
+
+            return _response;
+        }
+
+        [HttpGet("GetByCode/{code}")]
+        public ResponseDTO GetByCode (string code)
+        {
+            try
+            {
+                Coupon? obj = _dbContext.Coupons.FirstOrDefault(c => c.CouponCode.ToLower() == code.ToLower());
+                
+                if (obj == null)
+                {
+                    _response.Message = $"Coupon '{code}' not found";
+                }
+                else
+                {
+                    _response.Result = _mapper.Map<CouponDTO>(obj);
+                    _response.Success = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.GetType().ToString() + " ::: " + ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpPost]
+        public ResponseDTO Post ([FromBody]CouponDTO couponDTO)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(couponDTO);
+
+                _dbContext.Coupons.Add(obj);
+                _dbContext.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDTO>(obj);
+                _response.Success = true;
+            }
+            catch(Exception ex)
+            {
+                _response.Message = ex.GetType().ToString() + " ::: " + ex.Message;
+            }
+            
+            return _response;
+        }
+
+        [HttpPut]
+        public ResponseDTO Put ([FromBody]CouponDTO couponDTO)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(couponDTO);
+
+                if (obj == null)
+                {
+                    _response.Result = couponDTO;
+                    _response.Message = $"Coupon [{couponDTO.CouponId}] not found";
+                }
+                else
+                {
+                    _dbContext.Coupons.Update(obj);
+                    _dbContext.SaveChanges();
+
+                    _response.Result = _mapper.Map<CouponDTO>(obj);
+                    _response.Success = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.GetType().ToString() + " ::: " + ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpDelete("{id:int}")]
+        public ResponseDTO Delete (int id)
+        {
+            try
+            {
+                Coupon? obj = _dbContext.Coupons.FirstOrDefault(c => c.CouponId == id);
+
+                if (obj == null)
+                {
+                    _response.Message = $"Coupon [{id}] not found";
+                }
+                else
+                {
+                    _dbContext.Coupons.Remove(obj);
+                    _dbContext.SaveChanges();
+
+                    _response.Result = _mapper.Map<CouponDTO>(obj);
+                    _response.Success = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.GetType().ToString() + " ::: " + ex.Message;
+            }
+
+            return _response;
+        }
+    }
+}
