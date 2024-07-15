f56b4d6d290994b285a36e59671ca0739ba6dbaa
Clayton Golembiouski <claytong@iportsolutions.com.br>
2024-07-15 17:17:50 -0300

    BaseService basic implementation

--- Shopping.Web/Service/BaseService.cs
+++ Shopping.Web/Service/BaseService.cs
@@ -1,9 +1,7 @@
-﻿using Shopping.Web.Models;
-using Shopping.Web.Models.DTO;
+﻿using Shopping.Web.Models.DTO;
 using Shopping.Web.Service.IService;
 using System.Text.Json;
 using System.Text;
-using Microsoft.AspNetCore.Mvc;
 using static Shopping.Web.Enumerators.StaticDetails;
 
 namespace Shopping.Web.Service
@@ -11,6 +9,7 @@ namespace Shopping.Web.Service
     public class BaseService : IBaseService
     {
         private readonly IHttpClientFactory _httpClientFactory;
+
         public BaseService(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
@@ -18,33 +17,69 @@ namespace Shopping.Web.Service
 
         public async Task<ResponseDTO?> SendAsync(RequestDTO requestDTO)
         {
-            throw new NotImplementedException();
-
-            HttpClient client = _httpClientFactory.CreateClient("ShoppingAPI");
-            HttpRequestMessage message = new();
-            message.Headers.Add("Accept", "application/json");
-            //TODO: AddToken
-
-            message.RequestUri = new Uri(requestDTO.Url);
-
-            if (requestDTO.Data != null)
+            try
             {
-                message.Content = new StringContent(JsonSerializer.Serialize(requestDTO.Data), Encoding.UTF8, "application/json");
+                HttpClient client = _httpClientFactory.CreateClient("ShoppingAPI");
+
+                HttpRequestMessage message = new();
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(requestDTO.Url);
+
+                //TODO: AddToken
+
+                if (requestDTO.Data != null)
+                {
+                    message.Content = new StringContent(JsonSerializer.Serialize(requestDTO.Data), Encoding.UTF8, "application/json");
+                }
+
+                switch (requestDTO.ApiMethod)
+                {
+                    case ApiMethod.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+
+                    case ApiMethod.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+
+                    case ApiMethod.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                HttpResponseMessage? apiResponse = await client.SendAsync(message);
+
+                switch (apiResponse.StatusCode)
+                {
+                    case System.Net.HttpStatusCode.NotFound:
+                        return new() { Success = false, Message = "Not Found" };
+
+                    case System.Net.HttpStatusCode.Forbidden:
+                        return new() { Success = false, Message = "Access Denied" };
+
+                    case System.Net.HttpStatusCode.Unauthorized:
+                        return new() { Success = false, Message = "Unalthorized" };
+
+                    case System.Net.HttpStatusCode.InternalServerError:
+                        return new() { Success = false, Message = "Internal Server Error" };
+
+                    default:
+                        var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                        return JsonSerializer.Deserialize<ResponseDTO>(apiContent);
+                }
             }
-
-            HttpResponseMessage? apiResponse = null;
-
-            switch (requestDTO.ApiMethod)
+            catch (Exception ex)
             {
-                case ApiMethod.POST:   message.Method = HttpMethod.Post; break;
-                case ApiMethod.PUT:    message.Method = HttpMethod.Put; break;
-                case ApiMethod.DELETE: message.Method = HttpMethod.Delete; break;
-                default: message.Method = HttpMethod.Get; break;
+                return new ResponseDTO
+                {
+                    Message = ex.Message.ToString(),
+                    Success = false
+                };
             }
-
-            apiResponse = await client.SendAsync(message);
-
-            //TODO: continue..
         }
     }
 }
