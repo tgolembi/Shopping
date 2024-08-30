ef93648394e888ea70c8d73c69b5be61541f336d
Clayton Golembiouski <claytong@iportsolutions.com.br>
2024-08-30 14:19:32 -0300

    FrontEnd - Auth Controllers and Views for Login and Register, and menu items

---
+++ Shopping.Web/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.Web.Models;
+using Shopping.Web.Service.IService;
+
+namespace Shopping.Web.Controllers
+{
+	public class AuthController : Controller
+	{
+		private readonly IAuthService _authService;
+
+		public AuthController (IAuthService authService)
+		{
+			_authService = authService;
+		}
+
+		[HttpGet]
+		public IActionResult Login ()
+		{
+			LoginRequestDTO login = new();
+			return View(login);
+		}
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
