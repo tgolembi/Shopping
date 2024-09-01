6e31b24098aa6ba7a68320f49c880127b140be05
Ton Golembiouski <tgolembi@gmail.com>
2024-09-01 16:10:08 -0300

    Frontend Login and Registration Controllers and Views

--- Shopping.Web/Controllers/AuthController.cs
+++ Shopping.Web/Controllers/AuthController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Shopping.Web.Enumerators;
 using Shopping.Web.Models;
 using Shopping.Web.Service.IService;
+using Shopping.Web.Tools;
 
 namespace Shopping.Web.Controllers
 {
@@ -20,10 +23,53 @@ namespace Shopping.Web.Controllers
 			return View(login);
 		}
 
+        [HttpPost]
+        public async Task<IActionResult> Login (LoginRequestDTO login)
+        {
+            ResponseDTO response = await _authService.LoginAsync(login);
+
+            if (response != null && response.Success)
+            {
+                LoginResponseDTO? loginResponse = JsonHelper.Deserialize<LoginResponseDTO>(response?.Result?.ToString());
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("CustomError", response?.Message);
+                 return View(login);
+            }
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
+            ViewBag.RoleList = StaticDetails.GetEnumSelectList<StaticDetails.Role>();
+
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register (RegistrationDTO registration)
+        {
+			ResponseDTO result = await _authService.RegisterAsync(registration);
+
+			if (result != null && result.Success)
+			{
+				if (string.IsNullOrWhiteSpace(registration.Role))
+				{
+					registration.Role = StaticDetails.Role.Customer.ToString();
+				}
+				ResponseDTO assignRole = await _authService.AssignRoleAsync(registration);
+				if (assignRole != null && assignRole.Success)
+				{
+					TempData["success"] = "Registration Successful";
+					return RedirectToAction(nameof(Login));
+				}
+			}
+
+            ViewBag.RoleList = StaticDetails.GetEnumSelectList<StaticDetails.Role>();
+
+            return View(registration);
         }
 
         [HttpGet]
