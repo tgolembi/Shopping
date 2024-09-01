ab7e90d8bb343f4e530546eec5490577236c9b17
Ton Golembiouski <tgolembi@gmail.com>
2024-09-01 16:10:03 -0300

    StaticDetalis enum Roles and Methods EnumSelectList and EnumDescription

--- Shopping.Web/Enumerators/StaticDetails.cs
+++ Shopping.Web/Enumerators/StaticDetails.cs
@@ -1,6 +1,10 @@
-﻿namespace Shopping.Web.Enumerators
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace Shopping.Web.Enumerators
 {
-    public class StaticDetails
+    public static class StaticDetails
     {
         public static string CouponAPIBase { get; set; }
         public static string AuthAPIBase { get; set; }
@@ -11,6 +15,38 @@
             POST,
             PUT,
             DELETE
+        }
+
+        public enum Role
+        {
+            [Description("Administrator")]
+            Admin,
+            [Description("Customer")]
+            Customer
+        }
+
+        public static List<SelectListItem> GetEnumSelectList<T> () where T : Enum
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>()
+                       .Select(e => new SelectListItem
+                       {
+                           Value = e.ToString(),
+                           Text = GetEnumDescription(e)
+                       }).ToList();
+        }
+
+        public static string GetEnumDescription (this Enum value)
+        {
+            FieldInfo? fi = value.GetType().GetField(value.ToString());
+            if (fi != null)
+            {
+                var attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+                if (attributes.Length > 0)
+                {
+                    return attributes[0].Description;
+                }
+            }
+            return value.ToString();
         }
     }
 }
