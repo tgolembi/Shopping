4f11b0be6a66447d6d8d08925fd14d0d9be32bd5
Ton Golembiouski <tgolembi@gmail.com>
2024-09-12 23:21:14 -0300

    Add Extensions - WebApplicationBuilder AddAppAuthentication

---
+++ Shopping.Services.CouponAPI/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Shopping.Services.CouponAPI.Extensions
+{
+	public static class WebApplicationBuilderExtensions
+	{
+		public static WebApplicationBuilder AddAppAuthentication (this WebApplicationBuilder builder)
+		{
+			var settings = builder.Configuration.GetSection("ApiSettings");
+			var secret = settings.GetValue<string>("Secret");
+			var issuer = settings.GetValue<string>("Issuer");
+			var audience = settings.GetValue<string>("Audience");
+
+			var key = Encoding.ASCII.GetBytes(secret);
+
+			builder.Services.AddAuthentication(a =>
+			{
+				a.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				a.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+			}).AddJwtBearer(j =>
+			{
+				j.TokenValidationParameters = new TokenValidationParameters
+				{
+					ValidateIssuerSigningKey = true,
+					IssuerSigningKey = new SymmetricSecurityKey(key),
+					ValidateIssuer = true,
+					ValidIssuer = issuer,
+					ValidAudience = audience,
+					ValidateAudience = true
+				};
+			});
+
+			return builder;
+		}
+	}
+}
--- Shopping.Services.CouponAPI/Program.cs
+++ Shopping.Services.CouponAPI/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Shopping.Services.CouponAPI;
 using Shopping.Services.CouponAPI.Data;
-using System.Text;
+using Shopping.Services.CouponAPI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -57,29 +57,7 @@ builder.Services.AddSwaggerGen(option =>
 	});
 });
 
-var settings = builder.Configuration.GetSection("ApiSettings");
-var secret = settings.GetValue<string>("Secret");
-var issuer = settings.GetValue<string>("Issuer");
-var audience = settings.GetValue<string>("Audience");
-
-var key = Encoding.ASCII.GetBytes(secret);
-
-builder.Services.AddAuthentication(a => 
-{
-	a.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-	a.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(j => 
-{
-	j.TokenValidationParameters = new TokenValidationParameters
-	{
-		ValidateIssuerSigningKey = true,
-		IssuerSigningKey = new SymmetricSecurityKey(key),
-		ValidateIssuer = true,
-		ValidIssuer = issuer,
-		ValidAudience = audience,
-		ValidateAudience = true
-	};
-});
+builder.AddAppAuthentication();
 
 var app = builder.Build();
 
