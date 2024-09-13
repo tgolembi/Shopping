54b6f9b7ad7697400918fd952675aac1075f4354
Clayton Golembiouski <claytong@iportsolutions.com.br>
2024-09-13 14:19:02 -0300

    Product builder.Services.AddDbContext and connection strings

--- Shopping.Services.ProductAPI/Program.cs
+++ Shopping.Services.ProductAPI/Program.cs
@@ -1,15 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using Shopping.Services.ProductAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+	string connectionStringName = string.Empty;
+
+	switch (Environment.MachineName)
+	{
+		case "ASUS702":
+			connectionStringName = "Localhost702";
+			break;
+		case "BR-NOT-DEV-17":
+			connectionStringName = "LocalHostIPSTong";
+			break;
+		default:
+			throw new Exception("This machine has no database connection string associated");
+	}
+
+	string? connectionString = builder.Configuration.GetConnectionString(connectionStringName);
+
+	options.UseSqlServer(connectionString);
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
